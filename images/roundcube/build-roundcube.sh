0050e60c55bbf9ad3dd336a4042cc425d3557c20
tyranron <tyranron@gmail.com>
2016-10-03 09:56:56 +0300

    bump up Roundcube version to 1.2.2

--- images/roundcube/build-roundcube.sh
+++ images/roundcube/build-roundcube.sh
@@ -6,13 +6,16 @@ RESULT_DIR=$BUILD_DIR/result/var/www
 set -x
 
 mkdir -p $RESULT_DIR
-cd $RESULT_DIR
-git clone https://github.com/roundcube/roundcubemail ./
-git checkout tags/$ROUNDCUBE_VER
+cd $BUILD_DIR
+wget https://github.com/roundcube/roundcubemail/releases/download/$ROUNDCUBE_VER/roundcubemail-$ROUNDCUBE_VER.tar.gz
+tar -xzf roundcubemail-$ROUNDCUBE_VER.tar.gz
+# remove target directory for renaming not moving into
+rm -rf $RESULT_DIR
+mv roundcubemail-$ROUNDCUBE_VER $RESULT_DIR
 
+cd $RESULT_DIR
 mv composer.json-dist composer.json
 composer install --no-dev --optimize-autoloader
-
-rm -rf .git .gitignore .tx installer Dockerfile
+rm -rf installer
 
 mv $BUILD_DIR/* /out/
